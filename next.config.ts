23f5c4df684267dfab7c8909862495b4dd8054e9
MSN <s.mytsyk@gmail.com>
2026-01-20 01:19:13 +0300

    feat: init Next.js (TS, App Router)

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ['http://localhost:3000', 'http://127.0.0.1:3000', 'http://172.24.160.1:3000'],
+  allowedDevOrigins: [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://172.24.160.1:3000",
+  ],
 };
 
 export default nextConfig;
